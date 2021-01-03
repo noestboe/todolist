4bca582e854347ae1e972cf555a0c0752fdb6abb
noestboe <noestboe@gmail.com>
2021-01-03 20:46:49 +0000

    ferdig oppsett test kjører

--- todolist/src/test/java/todolist/AppTest.java
+++ todolist/src/test/java/todolist/AppTest.java
@@ -1,11 +1,15 @@
 package todolist;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.apiTest;
+import org.junit.jupiter.api.Test;
 
 
 public class AppTest {
-    
+    public AppTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue( true );
