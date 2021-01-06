a05a3b616b3e3c72a52e7ad2dffa76bcaabac48c
noestboe <noestboe@gmail.com>
2021-01-06 16:35:56 +0000

    la til contoller og app

--- todolist/src/main/java/todolist/ui/App.java
+++ todolist/src/main/java/todolist/ui/App.java
@@ -3,6 +3,7 @@ package todolist.ui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -10,8 +11,12 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent parent = FXMLLoader.load(getClass().getResource("Todo.fxml"));
-        
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+    }
 
+    public static void main(String[] args) {
+        launch(App.class, args);
     }
     
 }
---
+++ todolist/src/main/java/todolist/ui/TodoController.java
@@ -0,0 +1,10 @@
+package todolist.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class TodoController {
+    
+    @FXML
+    private ListView listView;
+}
